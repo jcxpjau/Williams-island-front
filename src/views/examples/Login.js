0adad207fa04d3d5ec6215e0b8c69cd12c67a434
eduarda-pimentel <eduarda.pimentel001@gmail.com>
2025-06-30 16:11:24 -0300

    WI-59 #done: info durante o login

--- src/views/examples/Login.js
+++ src/views/examples/Login.js
@@ -11,6 +11,7 @@ import {
   InputGroup,
   Row,
   Col,
+  Spinner,
 } from "reactstrap";
 
 import { useState } from "react";
@@ -24,20 +25,25 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [rememberMe, setRememberMe] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function LoginAuth(e) {
     e.preventDefault();
+    setLoading(true);
     try {
       const res = await api.post("auth/login", { email, password });
-      console.log(res)
+
       if (res.data.access_token) {
         login(res.data.access_token, rememberMe);
-        navigate("/admin/index")
+        navigate("/admin/index");
+        setLoading(false);
       }
-    } catch(err){
-      console.log(err)
+    } catch (err) {
+      setError("Incorrect email or password");
+      setLoading(false);
     }
-  } 
+  }
 
   return (
     <>
@@ -59,7 +65,10 @@ const Login = () => {
                     placeholder="Email"
                     type="email"
                     value={email}
-                    onChange={(e)=>setEmail(e.target.value)}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                      setError(null);
+                    }}
                     autoComplete="new-email"
                   />
                 </InputGroup>
@@ -76,7 +85,10 @@ const Login = () => {
                     type="password"
                     autoComplete="new-password"
                     value={password}
-                    onChange={(e)=>setPassword(e.target.value)}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                      setError(null);
+                    }}
                   />
                 </InputGroup>
               </FormGroup>
@@ -86,7 +98,7 @@ const Login = () => {
                   id=" customCheckLogin"
                   type="checkbox"
                   checked={rememberMe}
-                  onClick={()=>setRememberMe(!rememberMe)}
+                  onClick={() => setRememberMe(!rememberMe)}
                 />
                 <label
                   className="custom-control-label"
@@ -95,15 +107,24 @@ const Login = () => {
                   <span className="text-white">Remember me</span>
                 </label>
               </div>
-              <div className="text-center">
-                <Button
-                  className="my-4"
-                  color="secondary"
-                  type="submit"
-                >
-                  Sign in
-                </Button>
-              </div>
+              {error && (
+                <div className="text-danger my-2">
+                  <span> {error} </span>
+                </div>
+              )}
+              {loading && (
+                <div className="d-flex flex-column align-items-center justify-content-center mt-4 text-white">
+                  <Spinner />
+                  <p className="mt-2"> Logging in </p>
+                </div>
+              )}
+              {!loading && (
+                <div className="text-center">
+                  <Button className="my-4" color="secondary" type="submit">
+                    Sign in
+                  </Button>
+                </div>
+              )}
             </Form>
           </CardBody>
         </Card>
@@ -120,7 +141,7 @@ const Login = () => {
           <Col className="text-right" xs="6">
             <a
               className="text-light"
-          /*     onClick={() => navigate("/auth/register")} */
+              /*     onClick={() => navigate("/auth/register")} */
             >
               <small style={{ color: "#172b4d" }}>Create new account</small>
             </a>
