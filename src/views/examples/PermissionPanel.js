8a2c346c7e9827fb8326eb83049ab5825dbbdc65
eduarda-pimentel <eduarda.pimentel001@gmail.com>
2025-07-07 10:55:04 -0300

    WI-57 #progress: layout de switches

--- src/views/examples/PermissionPanel.js
+++ src/views/examples/PermissionPanel.js
@@ -1,5 +1,16 @@
 import { useState } from "react";
-import { Card, CardHeader, Row } from "reactstrap";
+import { Form } from "react-router-dom";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Col,
+  CustomInput,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+} from "reactstrap";
 
 function PermissionPanel() {
   const [selectedRole, setSelectedRole] = useState("");
@@ -56,24 +67,82 @@ function PermissionPanel() {
   return (
     <Card>
       <CardHeader>
-        <Row>
-          <div>
+        <Row className="px-3">
+          <div className="d-flex flex-row align-items-center">
+            <h3 className="font-weight-normal m-0">
+              {" "}
+              Editing permissions for{" "}
+            </h3>
             <select
-              className="form-control"
+              className="form-control mx-2 border-0"
               value={selectedRole}
               onChange={(e) => setSelectedRole(e.target.value)}
-              style={{ width: "250px" }}
+              style={{
+                width: "250px",
+                fontWeight: "bold",
+                color: "#363660",
+                "font-size": "1rem",
+              }}
             >
-              <option value="" disabled>
-                Select a role
-              </option>
-              {roles.map((role) => {
-                return <option value={role.value}>{role.label}</option>;
-              })}
+              <option value=""></option>
+              {roles.map((role) => (
+                <option key={role.value} value={role.value}>
+                  {role.label}
+                </option>
+              ))}
             </select>
           </div>
         </Row>
       </CardHeader>
+      <CardBody className="border">
+        <Row md="6" className="border-bottom">
+          <Col md="2" className=" p-2">
+            <h3>Entities</h3>
+          </Col>
+          <Col md="10" className="text-center  p-2">
+            <h3>Permissions</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col md="2" className="border-right p-3"></Col>
+          <Col md="2" className="border-right p-3">
+            <h3 className="font-weight-normal"> Create </h3>
+          </Col>
+          <Col md="2" className="border-right border-right p-3">
+            <h3 className="font-weight-normal"> Read </h3>
+          </Col>
+          <Col md="2" className="border-right p-3">
+            <h3 className="font-weight-normal"> Update </h3>
+          </Col>
+          <Col md="2" className="border-right p-3">
+            <h3 className="font-weight-normal"> Delete </h3>
+          </Col>
+          <Col md="2" className="border-right p-3">
+            <h3 className="font-weight-normal"> All </h3>
+          </Col>
+        </Row>
+        {selectedRole && areas.map((area) => (
+          <Row className="border-bottom" key={area.id}>
+            <Col md="2" className="p-3 border-right d-flex align-items-center">
+              {area.label}
+            </Col>
+
+            {[1, 2, 3, 4, 5].map((num) => (
+              <Col
+                md="2"
+                key={`switch-${num}-${area.id}`}
+                className="p-3 border-right d-flex justify-content-center align-items-center"
+              >
+                <CustomInput
+                  type="switch"
+                  id={`switch-${num}-${area.id}`}
+                  label=""
+                />
+              </Col>
+            ))}
+          </Row>
+        ))}
+      </CardBody>
     </Card>
   );
 }
